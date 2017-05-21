b2965810d7cfa93120e19f007cf1661d7f876ec0
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2017-05-22 01:16:56 +0200

    Create initial home page design with dummy data
    Provided dummy data into 'home.controller.js'.
    Created initial home page sidebar with hardcoded categories.

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -9,5 +9,38 @@
 
     function HomeController() {
         var homeVm = this;
+
+        homeVm.products = [
+            {
+                "preview": "https://supersoccershop.com/image/cache/catalog/jersey-16-17/real-madrid/real-madrid-sergio-ramos-jersey-2016-17-soccer-shirt-kit-600x600.jpg",
+                "name": "Sergio Ramos Home Jersey 2016/17",
+                "price": 21.10
+            },
+            {
+                "preview": "http://www.footballjersey.store/images/real-madrid/sergio-ramos-real-madrid-16-17-white-long-sleeve-jersey.jpg",
+                "name": "Sergio Ramos Home Jersey Long Sleeve",
+                "price": 42.44
+            },
+            {
+                "preview": "http://www.footballjersey.store/images/real-madrid/sergio-ramos-2016-17-third-black.jpg",
+                "name": "Sergio Ramos Away Jersey 2016/17",
+                "price": 15.20
+            },
+            {
+                "preview": "http://www.spainsocceronline.com/images/products/soccer_jerseys/nation/spain/nation_spain_485.jpg",
+                "name": "Sergio Ramos National Jersey Woman",
+                "price": 33.12
+            },
+            {
+                "preview": "http://www.footballjersey.store/images/real-madrid/sergio-ramos-real-madrid-16-17-white-long-sleeve-jersey.jpg",
+                "name": "Sergio Ramos Home Jersey Long Sleeve",
+                "price": 42.44
+            },
+            {
+                "preview": "http://www.footballjersey.store/images/real-madrid/sergio-ramos-2016-17-third-black.jpg",
+                "name": "Sergio Ramos Away Jersey 2016/17",
+                "price": 15.20
+            }
+        ]
     }
 })();
