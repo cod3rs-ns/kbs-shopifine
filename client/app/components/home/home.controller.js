997ee9035bd4aad97e812e01dafe23b6f3d85724
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2017-05-22 00:18:54 +0200

    Intial Home page template
    Specified home route. Added 'bootstrap' to 'bower.json' dependecies.
    Provided 'custom-style.css'.

---
+++ client/app/components/home/home.controller.js
@@ -0,0 +1,13 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('shopifine-app')
+        .controller('HomeController', HomeController);
+
+    HomeController.$inject = [];
+
+    function HomeController() {
+        var homeVm = this;
+    }
+})();
