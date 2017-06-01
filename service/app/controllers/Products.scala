3e83bdfca8b1e392dcae24a6021ca5820e1367c9
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2017-06-01 17:58:23 +0200

    Remove stubbed endpoints

--- service/app/controllers/Products.scala
+++ service/app/controllers/Products.scala
@@ -2,17 +2,13 @@ package controllers
 
 import javax.inject._
 
-import domain.{Product, ProductCategory}
-import org.joda.time.DateTime
 import play.api.libs.json.Json
 import play.api.mvc._
-import products.{ProductCollectionResponse, ProductResponse}
 
 import scala.concurrent.Future
 
 @Singleton
 class Products @Inject() extends Controller {
-  import hateoas.JsonApi._
 
   def add(): Action[AnyContent] = Action.async {
     Future.successful(Ok(Json.toJson("Added!")))
@@ -22,60 +18,14 @@ class Products @Inject() extends Controller {
     Future.successful(Ok(Json.toJson(id)))
   }
 
-  def retrieveAll(`page[limit]`: Int, `page[offset]`: Int): Action[AnyContent] = Action.async {
+  def retrieveAll(limit: Int, offset: Int): Action[AnyContent] = Action.async {
     implicit request => {
-
-      val limit = `page[limit]`
-      val offset = `page[offset]`
-
-      val category = ProductCategory(
-        id = Some(1),
-        name = "Product Category",
-        maximumDiscount = 42.0
-      )
-
-      val p1 = Product(
-        id = Some(1),
-        name = "Product 1",
-        category = category,
-        price = 11,
-        quantity = 22,
-        createdAt = DateTime.now,
-        minQuantity = 0
-      )
-
-      val p2 = Product(
-        id = Some(2),
-        name = "Product 2",
-        category = category,
-        price = 11,
-        quantity = 22,
-        createdAt = DateTime.now,
-        minQuantity = 0
-      )
-
-      Future.successful(Ok(Json.toJson(ProductCollectionResponse.fromDomain(Seq(p1, p2)))))
+      Future.successful(Ok("ok"))
     }
   }
 
   def retrieveOne(id: Long): Action[AnyContent] = Action.async {
-    val category = ProductCategory(
-      id = Some(1),
-      name = "Product Category",
-      maximumDiscount = 42.0
-    )
-
-    val product = Product(
-      id = Some(1),
-      name = "Product 1",
-      category = category,
-      price = 11,
-      quantity = 22,
-      createdAt = DateTime.now,
-      minQuantity = 0
-    )
-
-    Future.successful(Ok(Json.toJson(ProductResponse.fromDomain(product))))
+    Future.successful(Ok("ok"))
   }
 
   def fillStock(id: Long, quantity: Int): Action[AnyContent] = Action.async {
