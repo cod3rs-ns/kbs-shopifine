f67ca948678508ff27f4f7aefe145010c0c2afd1
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2017-06-24 20:09:04 +0200

    Specify stubbed endpoints for Consumption Thresholds

--- service/app/controllers/BuyerCategories.scala
+++ service/app/controllers/BuyerCategories.scala
@@ -1,7 +1,8 @@
 package controllers
 
 import commons.CollectionLinks
-import hateoas.buyer_categories.{BuyerCategoryCollectionResponse, BuyerCategoryRequest, BuyerCategoryResponse}
+import domain.ConsumptionThreshold
+import hateoas.buyer_categories._
 import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{Action, AnyContent, Controller}
 
@@ -15,7 +16,10 @@ class BuyerCategories extends Controller {
     request.body.validate[BuyerCategoryRequest].fold(
       failures => Future.successful(BadRequest(Json.toJson("Malformed JSON specified."))),
 
-      spec => Future.successful(Ok(Json.toJson(BuyerCategoryResponse.fromDomain(spec.toDomain.copy(id = Some(1))))))
+      spec => Future.successful(
+        Ok(Json.toJson(
+          BuyerCategoryResponse.fromDomain(spec.toDomain.copy(id = Some(1))))
+        ))
     )
   }
 
@@ -27,16 +31,36 @@ class BuyerCategories extends Controller {
     )
   }
 
-  def retrieveThresholds(id: Long, offset: Int, limit: Int): Action[AnyContent] = Action.async {
-    Future.successful(Ok("ok"))
+  def retrieveThresholds(buyerCategoryId: Long, offset: Int, limit: Int): Action[AnyContent] = Action.async {
+    val thresholds = Seq(
+      ConsumptionThreshold(id = Some(1), from = 1, to = 10, award = 20.25),
+      ConsumptionThreshold(id = Some(2), from = 11, to = 39, award = 40.0)
+    )
+
+    Future.successful(
+      Ok(Json.toJson(
+        ConsumptionThresholdCollectionResponse.fromDomain(thresholds, CollectionLinks("self", Some("next")))
+      ))
+    )
   }
 
-  def addThreshold(id: Long): Action[AnyContent] = Action.async {
-    Future.successful(Ok("ok"))
+  def addThreshold(id: Long): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    request.body.validate[ConsumptionThresholdRequest].fold(
+      failures => Future.successful(BadRequest(Json.toJson("Malformed JSON specified."))),
+
+      spec => Future.successful(
+        Ok(Json.toJson(
+          ConsumptionThresholdResponse.fromDomain(spec.toDomain.copy(id = Some(1))))
+        ))
+    )
   }
 
   def removeThreshold(categoryId: Long, thresholdId: Long): Action[AnyContent] = Action.async {
-    Future.successful(Ok("ok"))
+    // Bad Request  If category doesn't exist
+    // Forbidden    If no privileges
+    // Not Found    If threshold doesn't exist
+    // No Content   Successfully deleted
+    Future.successful(NoContent)
   }
 
 }
