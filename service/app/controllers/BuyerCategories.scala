83107213cad4cbe10ddc23008ac1901c42e890ae
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2017-06-25 13:03:25 +0200

    Expose endpoints for Buyer Categories and Consumption Thresholds
    Exposed endpoints with basic repository create, read and delete
    operations both for buyer categories and consumption thresholds.

--- service/app/controllers/BuyerCategories.scala
+++ service/app/controllers/BuyerCategories.scala
@@ -1,14 +1,19 @@
 package controllers
 
+import javax.inject.Singleton
+
+import com.google.inject.Inject
 import commons.CollectionLinks
-import domain.ConsumptionThreshold
 import hateoas.buyer_categories._
 import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{Action, AnyContent, Controller}
+import repositories.{BuyerCategoryRepository, ConsumptionThresholdRepository}
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-class BuyerCategories extends Controller {
+@Singleton
+class BuyerCategories @Inject()(buyerCategories: BuyerCategoryRepository, thresholds: ConsumptionThresholdRepository)
+                               (implicit val ec: ExecutionContext) extends Controller {
 
   import hateoas.JsonApi._
 
@@ -16,42 +21,44 @@ class BuyerCategories extends Controller {
     request.body.validate[BuyerCategoryRequest].fold(
       failures => Future.successful(BadRequest(Json.toJson("Malformed JSON specified."))),
 
-      spec => Future.successful(
-        Ok(Json.toJson(
-          BuyerCategoryResponse.fromDomain(spec.toDomain.copy(id = Some(1))))
-        ))
+      spec => {
+        buyerCategories.save(spec.toDomain).map({
+          category => Ok(Json.toJson(BuyerCategoryResponse.fromDomain(category)))
+        })
+      }
     )
   }
 
-  def retrieveAll(offset: Int, limit: Int): Action[AnyContent] = Action.async {
-    Future.successful(
+  def retrieveAll(offset: Int, limit: Int): Action[AnyContent] = Action.async { implicit request =>
+    buyerCategories.retrieveAll(offset, limit).map(categories => {
+      val self = routes.BuyerCategories.retrieveAll(offset, limit).absoluteURL()
+      val next = if (limit == categories.length) Some(routes.BuyerCategories.retrieveAll(offset + limit, limit).absoluteURL()) else None
+
       Ok(Json.toJson(
-        BuyerCategoryCollectionResponse.fromDomain(Seq(), CollectionLinks("self", Some("next")))
+        BuyerCategoryCollectionResponse.fromDomain(categories, CollectionLinks(self, next))
       ))
-    )
+    })
   }
 
-  def retrieveThresholds(buyerCategoryId: Long, offset: Int, limit: Int): Action[AnyContent] = Action.async {
-    val thresholds = Seq(
-      ConsumptionThreshold(id = Some(1), from = 1, to = 10, award = 20.25),
-      ConsumptionThreshold(id = Some(2), from = 11, to = 39, award = 40.0)
-    )
+  def retrieveThresholds(buyerCategoryId: Long, offset: Int, limit: Int): Action[AnyContent] = Action.async { implicit request =>
+    thresholds.retrieveByBuyerCategory(buyerCategoryId, offset, limit).map(result => {
+      val self = routes.BuyerCategories.retrieveThresholds(buyerCategoryId, offset, limit).absoluteURL()
+      val next = if (limit == result.length) Some(routes.BuyerCategories.retrieveThresholds(buyerCategoryId, offset + limit, limit).absoluteURL()) else None
 
-    Future.successful(
       Ok(Json.toJson(
-        ConsumptionThresholdCollectionResponse.fromDomain(thresholds, CollectionLinks("self", Some("next")))
+        ConsumptionThresholdCollectionResponse.fromDomain(result, CollectionLinks(self, next))
       ))
-    )
+    })
   }
 
   def addThreshold(id: Long): Action[JsValue] = Action.async(parse.json) { implicit request =>
     request.body.validate[ConsumptionThresholdRequest].fold(
       failures => Future.successful(BadRequest(Json.toJson("Malformed JSON specified."))),
 
-      spec => Future.successful(
-        Ok(Json.toJson(
-          ConsumptionThresholdResponse.fromDomain(spec.toDomain.copy(id = Some(1))))
-        ))
+      spec =>
+        thresholds.save(spec.toDomain).map({
+          threshold => Ok(Json.toJson(ConsumptionThresholdResponse.fromDomain(threshold)))
+        })
     )
   }
 
@@ -60,7 +67,10 @@ class BuyerCategories extends Controller {
     // Forbidden    If no privileges
     // Not Found    If threshold doesn't exist
     // No Content   Successfully deleted
-    Future.successful(NoContent)
+    thresholds.delete(thresholdId).map(
+      affected =>
+        if (affected > 0) NoContent else NotFound("Threshold not found")
+    )
   }
 
 }
