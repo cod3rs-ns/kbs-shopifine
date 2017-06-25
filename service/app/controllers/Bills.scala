5a19930af0ab0252a1a26e9ee1cb52111fe32422
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2017-06-25 23:45:41 +0200

    Expose Bills endpoints without error handling
    Created Bill related endpoints without error handling. Need to improve
    response and creating with linking to BillItems.

--- service/app/controllers/Bills.scala
+++ service/app/controllers/Bills.scala
@@ -2,20 +2,73 @@ package controllers
 
 import javax.inject.Singleton
 
+import bills.{BillCollectionResponse, BillRequest, BillResponse}
 import com.google.inject.Inject
-import play.api.mvc.Controller
+import commons.CollectionLinks
+import domain.BillState
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{Action, AnyContent, Controller}
+import repositories.{BillItemRepository, BillRepository}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class Bills @Inject()()(implicit val ec: ExecutionContext) extends Controller {
+class Bills @Inject()(bills: BillRepository, billItems: BillItemRepository)
+                     (implicit val ec: ExecutionContext) extends Controller {
 
-  def create(userId: Long) = ???
+  import hateoas.JsonApi._
 
-  def retrieveAllByUser(userId: Long, offset: Int, limit: Int) = ???
+  def create(userId: Long): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    request.body.validate[BillRequest].fold(
+      failures => Future.successful(BadRequest(Json.toJson("Malformed JSON specified."))),
 
-  def retrieveAll(offset: Int, limit: Int) = ???
+      spec => {
+        bills.save(spec.toDomain).map({
+          bill => Created(Json.toJson(BillResponse.fromDomain(bill)))
+        })
+      }
+    )
+  }
 
-  def changeState(id: Long, state: String) = ???
+  def retrieveAllByUser(userId: Long, offset: Int, limit: Int): Action[AnyContent] = Action.async { implicit request =>
+    bills.retrieveByUser(userId, offset, limit).map({ result =>
+      val self = routes.Bills.retrieveAllByUser(userId, offset, limit).absoluteURL()
+      val next = if (result.size == limit) Some(routes.Bills.retrieveAllByUser(userId, offset, limit).absoluteURL()) else None
+
+      Ok(Json.toJson(
+        BillCollectionResponse.fromDomain(result, CollectionLinks(self, next))
+      ))
+    })
+  }
+
+  def retrieveAll(offset: Int, limit: Int): Action[AnyContent] = Action.async { implicit request =>
+    bills.retrieveAll(offset, limit).map({ result =>
+      val self = routes.Bills.retrieveAll(offset, limit).absoluteURL()
+      val next = if (result.size == limit) Some(routes.Bills.retrieveAll(offset, limit).absoluteURL()) else None
+
+      Ok(Json.toJson(
+        BillCollectionResponse.fromDomain(result, CollectionLinks(self, next))
+      ))
+    })
+  }
+
+  def changeState(id: Long, state: String): Action[AnyContent] = Action.async {
+    // FIXME Handle error
+    val toState = BillState.valueOf(state.toUpperCase)
+
+    bills.setState(id, toState).flatMap(
+      affected => {
+        // FIXME Handle Not Found
+        if (affected > 0) {
+          bills.retrieveOne(id).map({ bill =>
+            Ok(Json.toJson(BillResponse.fromDomain(bill.get)))
+          })
+        }
+        else {
+          Future.successful(NotFound("test-message"))
+        }
+      }
+    )
+  }
 
 }
