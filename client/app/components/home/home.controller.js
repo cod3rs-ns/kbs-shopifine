1e31de9802758d218d23c7213c5e5dc79f78cf66
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2017-07-27 01:06:31 +0200

    Home page design with products retrieving

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -5,42 +5,40 @@
         .module('shopifine-app')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = [];
+    HomeController.$inject = ['$log', 'CONFIG', '_', 'productService'];
 
-    function HomeController() {
+    function HomeController($log, CONFIG, _, productService) {
         var homeVm = this;
 
-        homeVm.products = [
-            {
-                "preview": "https://supersoccershop.com/image/cache/catalog/jersey-16-17/real-madrid/real-madrid-sergio-ramos-jersey-2016-17-soccer-shirt-kit-600x600.jpg",
-                "name": "Sergio Ramos Home Jersey 2016/17",
-                "price": 21.10
-            },
-            {
-                "preview": "http://www.footballjersey.store/images/real-madrid/sergio-ramos-real-madrid-16-17-white-long-sleeve-jersey.jpg",
-                "name": "Sergio Ramos Home Jersey Long Sleeve",
-                "price": 42.44
-            },
-            {
-                "preview": "http://www.footballjersey.store/images/real-madrid/sergio-ramos-2016-17-third-black.jpg",
-                "name": "Sergio Ramos Away Jersey 2016/17",
-                "price": 15.20
-            },
-            {
-                "preview": "http://www.spainsocceronline.com/images/products/soccer_jerseys/nation/spain/nation_spain_485.jpg",
-                "name": "Sergio Ramos National Jersey Woman",
-                "price": 33.12
-            },
-            {
-                "preview": "http://www.footballjersey.store/images/real-madrid/sergio-ramos-real-madrid-16-17-white-long-sleeve-jersey.jpg",
-                "name": "Sergio Ramos Home Jersey Long Sleeve",
-                "price": 42.44
-            },
-            {
-                "preview": "http://www.footballjersey.store/images/real-madrid/sergio-ramos-2016-17-third-black.jpg",
-                "name": "Sergio Ramos Away Jersey 2016/17",
-                "price": 15.20
-            }
-        ]
+        homeVm.products = [];
+
+        homeVm.retrieveProducts = retrieveProducts;
+
+        init();
+
+        function init() {
+            var url = CONFIG.SERVICE_URL + '/products';
+            homeVm.retrieveProducts(url)
+        }
+
+        function retrieveProducts(url) {
+            productService.retrieveAllFrom(url)
+                .then(function (response) {
+                    $log.info(response);
+                    homeVm.products = _.map(response.data, function (product) {
+                        return {
+                            'name': product.attributes.name,
+                            'price': product.attributes.price,
+                            // FIXME Extend Product model with image
+                            'preview': "https://supersoccershop.com/image/cache/catalog/jersey-16-17/real-madrid/real-madrid-sergio-ramos-jersey-2016-17-soccer-shirt-kit-600x600.jpg"
+                        }
+                    });
+
+                    $log.info(response.links.next);
+                })
+                .catch(function (data) {
+                    $log.error(data);
+                });
+        }
     }
 })();
