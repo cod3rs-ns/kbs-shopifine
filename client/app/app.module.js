060c739794f92cb9abab2dc00bed349aaa6f5575
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2017-07-26 23:47:45 +0200

    Implement registration on front-end

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -2,5 +2,6 @@ angular
     .module('shopifine-app', [
         'ui.router',
         'ngStorage',
+        'ngToast',
         'angular-jwt'
     ]);
--- client/app/components/register/register.controller.js
+++ client/app/components/register/register.controller.js
@@ -5,9 +5,69 @@
         .module('shopifine-app')
         .controller('RegisterController', RegisterController);
 
-    RegisterController.$inject = [];
+    RegisterController.$inject = ['ngToast', '_', 'usersService'];
 
-    function RegisterController() {
+    function RegisterController(ngToast, _, usersService) {
         var registerVm = this;
+
+        // FIXME Extract toDto function
+        registerVm.error = false;
+        registerVm.passwordRepeat = "";
+        registerVm.user = {
+            "data": {
+                "type": "users",
+                "attributes": {
+                    "username": "",
+                    "password": "",
+                    "firstName": "",
+                    "lastName": "",
+                    "role": "CUSTOMER",
+                    "address": null
+                }
+            }
+        };
+
+        registerVm.relationships = {
+            "buyerCategory": {
+                "data": {
+                    "type": "buyer-categories",
+                    "id": null
+                }
+            }
+        };
+
+        registerVm.register = register;
+        registerVm.isCustomer = isCustomer;
+
+        function register() {
+            if (registerVm.passwordRepeat !== registerVm.user.data.attributes.password) {
+                registerVm.error = true;
+                registerVm.message = "Password do not match!";
+                return;
+            }
+
+            // FIXME Better implementation
+            if (!isCustomer()) {
+                registerVm.user.data.relationships = null;
+                registerVm.user.data.attributes.address = null;
+            }
+
+            usersService.register(registerVm.user)
+                .then(function (response) {
+                    registerVm.error = false;
+
+                    ngToast.success({
+                        content: 'Welcome to Shopifine, ' + response.data.attributes.firstName + ' ' + response.data.attributes.lastName + '. Now you can go to login page.'
+                    });
+                })
+                .catch(function (data) {
+                    registerVm.error = true;
+                    registerVm.message = _.first(data.errors).detail;
+                });
+        }
+
+        function isCustomer() {
+            return !_.isEmpty(registerVm.user.data.attributes.role) && "CUSTOMER" === registerVm.user.data.attributes.role;
+        }
     }
 })();
