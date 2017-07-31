4d366502d486aee3b9b4d94150233e88926bbe84
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2017-08-01 00:58:29 +0200

    Add table of bills to customer profile

--- client/app/components/user/user-profile.controller.js
+++ client/app/components/user/user-profile.controller.js
@@ -5,14 +5,15 @@
         .module('shopifine-app')
         .controller('UserProfileController', UserProfileController);
 
-    UserProfileController.$inject = ['$log', '$localStorage', 'users'];
+    UserProfileController.$inject = ['$log', '$localStorage', 'users', 'bills'];
 
-    function UserProfileController($log, $localStorage, users) {
+    function UserProfileController($log, $localStorage, users, bills) {
         var profileVm = this;
 
-        profileVm.user = {};
+        profileVm.user = undefined;
 
         profileVm.me = me;
+        profileVm.retrieveBills = retrieveBills;
 
         init();
 
@@ -24,8 +25,31 @@
             var id = $localStorage.user.id;
             users.findBy(id)
                 .then(function (response) {
-                    $log.info(response);
                     profileVm.user = response.data.attributes;
+                    if (response.data.attributes.role === 'CUSTOMER') {
+                        profileVm.user.bills = [];
+                        retrieveBills(id);
+                    }
+                })
+                .catch(function (data) {
+                    $log.error(data);
+                });
+        }
+
+        function retrieveBills(id) {
+            bills.getBillsByUser(id)
+                .then(function (response) {
+                    _.forEach(response.data, function(bill) {
+                        profileVm.user.bills.push({
+                            'createdAt': bill.attributes.createdAt,
+                            'amount': bill.attributes.amount,
+                            'discount': bill.attributes.discount,
+                            'discountAmount': bill.attributes.discountAmount,
+                            'pointsGained': bill.attributes.pointsGained,
+                            'pointsSpent': bill.attributes.pointsSpent,
+                            'status': bill.attributes.state
+                        });
+                    });
                 })
                 .catch(function (data) {
                     $log.error(data);
