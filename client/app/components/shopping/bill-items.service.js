339912c050267e1c297dada51bfb4cd45594f22f
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2017-08-14 01:03:37 +0200

    Trigger bill creating process
    When user gets to its shopping cart and click on checkout button we need
    to create Bill along with all specified items.

---
+++ client/app/components/shopping/bill-items.service.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('shopifine-app')
+        .service('billItems', billItems);
+
+    billItems.$inject = ['$http', 'CONFIG'];
+
+    function billItems($http, CONFIG) {
+        var service = {
+            create: create
+        };
+
+        return service;
+
+        function create(userId, billId, billItem) {
+            return $http.post(CONFIG.SERVICE_URL + '/users/' + userId + '/bills/' + billId + '/bill-items', billItem)
+                .then(function success(response) {
+                    return response.data;
+                })
+                .catch(function error(response) {
+                    throw response.data;
+                });
+        }
+
+    }
+})();
